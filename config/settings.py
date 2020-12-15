834479066e242fdd656afc309c6f23f7782f2f38
sevenisalie <nolan.e.terry@gmail.com>
2020-12-14 22:48:55 -0600

    asdf

--- config/settings.py
+++ config/settings.py
@@ -180,11 +180,10 @@ DATABASES['default'].update(db_from_env)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATIC_ROOT = 'config/static'
-
-STATICFILES_DIRS =[
-os.path.join(BASE_DIR, 'static')
-
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
